ae3c2685f3651560b984f54b01e937357e5c7c03
1006mjkim <1006mjkim@gmail.com>
2025-04-08 20:40:00 +0900

    fetch + decode functions (partially)

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.swing.*;
 import java.awt.event.*;
 
 class Main implements ActionListener{
     static String code = "";
     static int PC = 0;
-    static int icode, ifun, rA, rB, valC, valP = 0;
+    // decide whether to make icode, ifun, rA, rB, valC, valP global or local
+    char icode = code.charAt(PC);
+    char ifun = code.charAt(PC+1);
 
     public static void main(String args[]) throws IOException{
         FileReader fr = new FileReader("file.txt");
@@ -35,8 +39,7 @@ class Main implements ActionListener{
     }
 
     public void fetch(String code){
-        char icode = code.charAt(PC);
-        char ifun = code.charAt(PC+1);
+        int rA = 0, rB = 0, valC = 0, valP = 0;
         switch(icode){
             case '0':
             case '1':
@@ -47,8 +50,57 @@ class Main implements ActionListener{
             case '3':
                 rA = 15;
                 rB = Integer.parseInt(String.valueOf(code.charAt(PC+3)), 16);
-                //valC = 
+                valC = Integer.parseInt(code.substring(2), 16);
+                valP = PC + 10;
+            case '4':
+            case '5':
+            case '6':
+                
+            case '7':
+                valC = Integer.parseInt(code.substring(2), 16);
+                valP = valC;
+            case '8':
+                valC = Integer.parseInt(code.substring(2), 16);
+                valP = valC;
+            case '9':
+                
+            case 'A':
+
+            case 'B':
         }
+        decode(rA, rB, valC, valP);
     }
     
+    public void decode(int rA, int rB, int valC, int valP){
+        int valA = 0, valB = 0;
+        switch(icode){
+            case '2':
+                valA = registers.get(rA);
+                valB = registers.get(rB);
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case 'A':
+            case 'B':
+        }
+    }
+
+    static HashMap<Integer, Integer> registers = new HashMap<Integer, Integer>(){{
+        put(0, 0);
+        put(1, 0);
+        put(2, 0);
+        put(3, 0);
+        put(4, 0);
+        put(5, 0);
+        put(6, 0);
+        put(7, 0);
+        put(8, 0);
+        put(9, 0);
+        put(10, 0);
+        put(11, 0);
+        put(12, 0);
+        put(13, 0);
+        put(14, 0);
+    }};
 }
